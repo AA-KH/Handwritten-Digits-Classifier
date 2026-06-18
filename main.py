2117af6776f903e58f311438996bb17a44be7cb0
AA-KH <ak.luxa@gmail.com>
2026-06-19 03:35:34 +0530

    optimized speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import struct
+from numba import njit
 
 def load_mnist_images(filename):
     with open(filename, 'rb') as f:
@@ -25,7 +26,7 @@ def convolve(image, kernel, stride = 1, padding = 0):
     if padding > 0:
         image = np.pad(image, ((padding, padding), (padding, padding)), mode="constant")
     output_size = ((image.shape[0] - kernel.shape[0])//stride) + 1
-    convolve_out = np.zeros((output_size, output_size))
+    convolve_out = np.zeros((output_size, output_size), dtype=np.float32)
 
     for i in range(output_size):
         for j in range(output_size):
@@ -53,7 +54,7 @@ def relu(feature_maps):
 #3. POOLING - TAKING MAX VALUE FROM NON OVERLAPPING PATCHES
 def max_pool(relu_out, pool_size = 2, stride = 2):
     output_size = ((relu_out.shape[0] - pool_size) // stride) + 1
-    pool_out = np.zeros((output_size, output_size))
+    pool_out = np.zeros((output_size, output_size), dtype=np.float32)
 
     for i in range(output_size):
         for j in range(output_size):
@@ -124,7 +125,7 @@ def flatten_backward(flattened_grad, pool_shape):
 
 #4. POOLING BACKWARD
 def max_pool_backward(relu_out, pool_out_grad, pool_size = 2, stride = 2):
-    relu_out_grad = np.zeros_like(relu_out, dtype=float)
+    relu_out_grad = np.zeros_like(relu_out, dtype=np.float32)
     output_size = pool_out_grad.shape[0]
 
     for i in range(output_size):
@@ -160,8 +161,8 @@ def convolve_backward(image, kernel, conv_out_grad, stride = 1, padding = 0):
         padded_image = np.pad(image,((padding, padding),(padding, padding)),mode="constant")
     else:
         padded_image = image
-    dKernel = np.zeros_like(kernel, dtype=float)
-    dInput = np.zeros_like(padded_image, dtype=float)
+    dKernel = np.zeros_like(kernel, dtype=np.float32)
+    dInput = np.zeros_like(padded_image, dtype=np.float32)
 
     output_size = conv_out_grad.shape[0]
 
@@ -178,10 +179,10 @@ def convolve_backward(image, kernel, conv_out_grad, stride = 1, padding = 0):
     return dKernel, dInput
 
 #MULTI KERNEL BACKWARD CONVOLUTION
-def conv_layer_backward(image, kernels, feature_maps_grad, stride = 1, padding = 0):
-    kernels_grad = np.zeros_like(kernels, dtype=float)
-    conv_biases_grad = np.zeros(len(kernels))
-    image_grad = np.zeros_like(image, dtype=float)
+def conv_layer_backward(image, kernels, conv_biases, feature_maps_grad, stride = 1, padding = 0):
+    kernels_grad = np.zeros_like(kernels, dtype=np.float32)
+    conv_biases_grad = np.zeros_like(conv_biases, dtype=np.float32)
+    image_grad = np.zeros_like(image, dtype=np.float32)
 
     for kernel_index in range(len(kernels)):
         kernel_grad, single_image_grad = (convolve_backward(image, kernels[kernel_index], feature_maps_grad[kernel_index], stride, padding))
@@ -192,8 +193,8 @@ def conv_layer_backward(image, kernels, feature_maps_grad, stride = 1, padding =
 
     return kernels_grad, conv_biases_grad, image_grad
 
-def forward(image, kernels, W1, b1, W2, b2, y_true):
-    feature_maps = conv_layer_forward(image, kernels, stride=1, padding=0)
+def forward(image, kernels, conv_biases, W1, b1, W2, b2):
+    feature_maps = conv_layer_forward(image, kernels, conv_biases, stride=1, padding=0)
     relu_out = relu(feature_maps)
     pooled_maps = pool_layer_forward(relu_out, pool_size=2, stride=2)
     flattened = flatten(pooled_maps)
@@ -201,7 +202,6 @@ def forward(image, kernels, W1, b1, W2, b2, y_true):
     hidden_relu = relu(hidden_scores)
     output_scores = dense_forward(hidden_relu, W2, b2)
     y_pred = softmax(output_scores)
-    loss = cross_entropy(y_true, y_pred)
 
     cache = {
     "image": image,
@@ -213,9 +213,9 @@ def forward(image, kernels, W1, b1, W2, b2, y_true):
     "hidden_relu": hidden_relu
     }
 
-    return loss, y_pred, cache
+    return y_pred, cache
 
-def backward(y_pred, y_true, cache, kernels, W1, W2):
+def backward(y_pred, y_true, cache, kernels, conv_biases, W1, W2):
     score_grad = cross_entropy_backward(y_pred, y_true)
     dW2, db2, hidden_relu_grad = dense_backward(cache["hidden_relu"], W2, score_grad)
     hidden_score_grad = relu_backward(hidden_relu_grad, cache["hidden_scores"])
@@ -223,7 +223,7 @@ def backward(y_pred, y_true, cache, kernels, W1, W2):
     pooled_maps_grad = flatten_backward(flattened_grad, cache["pooled_maps"].shape)
     relu_out_grad = pool_layer_backward(cache["relu_out"], pooled_maps_grad, pool_size=2, stride=2)
     feature_maps_grad = relu_backward(relu_out_grad, cache["feature_maps"])
-    kernels_grad, conv_biases_grad, image_grad = conv_layer_backward(cache["image"], kernels, feature_maps_grad, stride=1, padding=0)
+    kernels_grad, conv_biases_grad, image_grad = conv_layer_backward(cache["image"], kernels, conv_biases, feature_maps_grad, stride=1, padding=0)
 
     return kernels_grad, conv_biases_grad, dW1, db1, dW2, db2
 
@@ -235,22 +235,128 @@ def update(kernels, W1, W2, b1, b2, kernels_grad, dW1, dW2, db1, db2, lr, conv_b
     kernels = kernels - lr * kernels_grad
     conv_biases -= lr * conv_biases_grad
     
-    return kernels, W1, b1, W2, b2
+    return kernels, conv_biases, W1, b1, W2, b2
 
 def one_hot(label):
-    vector = np.zeros(10)
+    vector = np.zeros(10, dtype=np.float32)
     vector[label] = 1
 
     return vector
 
 image = np.random.randn(28,28)
-kernels= np.random.randn(16,3,3) * np.sqrt(2/9)
-W1 = np.random.randn(2704, 64) * np.sqrt(2/2704)
-b1 = np.zeros(64)
-W2 = np.random.randn(64, 10) * np.sqrt(2/64)
-b2 = np.zeros(10)
-conv_biases = np.zeros(16)
+kernels= np.random.randn(16,3,3).astype(np.float32) * np.sqrt(2/9)
+W1 = np.random.randn(2704, 64).astype(np.float32) * np.sqrt(2/2704)
+b1 = np.zeros(64, dtype=np.float32)
+W2 = np.random.randn(64, 10).astype(np.float32) * np.sqrt(2/64)
+b2 = np.zeros(10, dtype=np.float32)
+conv_biases = np.zeros(16, dtype=np.float32)
+batch_size = 32
 
 train_images = load_mnist_images("train-images.idx3-ubyte")
-
 train_labels = load_mnist_labels("train-labels.idx1-ubyte")
+test_images = load_mnist_images("t10k-images.idx3-ubyte")
+test_labels = load_mnist_labels("t10k-labels.idx1-ubyte")
+train_labels_one_hot = np.eye(10, dtype=np.float32)[train_labels]
+
+train_images = train_images[:5000]
+train_labels = train_labels[:5000]
+train_labels_one_hot = train_labels_one_hot[:5000]
+
+best_accuracy = 0
+
+for epoch in range(20):
+
+    total_loss = 0
+    correct = 0
+
+    indices = np.random.permutation(len(train_images))
+
+    train_images = train_images[indices]
+    train_labels = train_labels[indices]
+    train_labels_one_hot = train_labels_one_hot[indices]
+
+    for batch_start in range(0,len(train_images),batch_size):
+
+        batch_images = train_images[batch_start:batch_start+batch_size]
+        batch_labels = train_labels[batch_start:batch_start+batch_size]
+        batch_one_hot = train_labels_one_hot[batch_start:batch_start+batch_size]
+
+        kernels_grad_sum = np.zeros_like(kernels, dtype=np.float32)
+        conv_biases_grad_sum = np.zeros_like(conv_biases, dtype=np.float32)
+        dW1_sum = np.zeros_like(W1, dtype=np.float32)
+        db1_sum = np.zeros_like(b1, dtype=np.float32)
+        dW2_sum = np.zeros_like(W2, dtype=np.float32)
+        db2_sum = np.zeros_like(b2, dtype=np.float32)
+
+        batch_loss = 0
+
+        for image, y_true, label in zip(batch_images, batch_one_hot, batch_labels):
+
+            y_pred, cache = forward(image, kernels, conv_biases, W1, b1, W2, b2)
+            loss = cross_entropy(y_true, y_pred)
+            kernels_grad, conv_biases_grad, dW1, db1, dW2, db2 = backward(y_pred, y_true, cache, kernels, conv_biases, W1, W2)
+
+            kernels_grad_sum += kernels_grad
+            conv_biases_grad_sum += conv_biases_grad
+
+            dW1_sum += dW1
+            db1_sum += db1
+
+            dW2_sum += dW2
+            db2_sum += db2
+
+            prediction = np.argmax(y_pred)
+            if prediction == label:
+                correct += 1
+
+            batch_loss += loss
+        
+        actual_batch_size = len(batch_images)
+
+        kernels_grad_sum /= actual_batch_size
+        conv_biases_grad_sum /= actual_batch_size
+
+        dW1_sum /= actual_batch_size
+        db1_sum /= actual_batch_size
+
+        dW2_sum /= actual_batch_size
+        db2_sum /= actual_batch_size
+
+        kernels, conv_biases, W1, b1, W2, b2 = update(kernels, W1, W2, b1, b2, kernels_grad_sum, dW1_sum, dW2_sum, db1_sum, db2_sum, 0.005, conv_biases, conv_biases_grad_sum)
+
+        total_loss += batch_loss / actual_batch_size
+
+    accuracy = correct / len(train_images)
+
+    print(f"Epoch {epoch}: " f"Loss={total_loss:.4f} " f"Accuracy={accuracy:.4f}")
+
+    if epoch % 5 == 0:
+
+        correct = 0
+
+        for image, label in zip(test_images, test_labels):
+
+            y_pred, _ = forward(image,kernels, conv_biases, W1, b1, W2, b2)
+
+            prediction = np.argmax(y_pred)
+            if prediction == label:
+                correct += 1
+
+        test_accuracy = (correct / len(test_images))
+
+        if test_accuracy > best_accuracy:
+            best_accuracy = test_accuracy
+
+            np.save("kernels.npy", kernels)
+            np.save("conv_biases.npy", conv_biases)
+            np.save("W1.npy", W1)
+            np.save("b1.npy", b1)
+            np.save("W2.npy", W2)
+            np.save("b2.npy", b2)
+
+            print(
+                f"Saved model "
+                f"({test_accuracy:.4f})"
+            )
+
+        print(f"Test Accuracy: " f"{test_accuracy:.4f}")
